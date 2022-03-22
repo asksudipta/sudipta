1a16db39da6dd773defe69a16da08505038c23db
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-22 15:10:34 +0100

    If condition test

---
+++ src/main/java/se/lexicon/sudipta/Exercise1.java
@@ -0,0 +1,22 @@
+package se.lexicon.sudipta;
+
+//class name should start with capital letter for each word
+public class Exercise1 {
+    // ctrl + alt + L 0 reformat codes
+    public static void main(String[] args) {
+
+        int i = 20;
+        int j = 10;
+        if (i >= j) {
+            System.out.println("this is good");
+        } else {
+            System.out.println("this is not good");
+
+        }
+    }
+
+
+}
+
+
+
