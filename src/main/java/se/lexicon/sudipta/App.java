540fa565f55f60eed4e9e28c13859a217b48b033
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-22 10:22:00 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/sudipta/App.java
@@ -0,0 +1,141 @@
+package se.lexicon.sudipta;
+
+
+public class App {
+    public static void main(String []args) {
+      /*
+       System.out.println( "Hello World!" );
+       Short age=100;
+       System.out.println(age);
+
+        int tax;
+        tax = 100;
+        System.out.println(tax);
+
+
+        long accountNumber;
+        accountNumber=12345678912345L;
+        long bankAccount=1645289263788389893L;
+        System.out.println("accountNumber:"+bankAccount);
+        System.out.println(accountNumber);
+
+
+
+        float floatvariable=1984.9387f;
+        double doublevariable=244635.2717836387d;
+
+        //why we cant not get the numbers in one row??
+       // System.out.println(floatvariable + doublevariable);
+
+        System.out.println(floatvariable);
+        System.out.println(doublevariable);
+
+
+
+//boolean
+        int num1=10;
+        int num2=5;
+        System.out.println(num1==num2);//false
+        System.out.println(num1!=num2);//true
+
+        boolean result=num1>num2;
+
+
+
+        int i=10;
+        i=i+10;
+        System.out.println(i);
+        System.out.println("i:"+i);
+        System.out.println(i++);
+        System.out.println(++i);
+
+
+
+    char test1='a';
+        char test='A';
+        boolean isequal=test==test1;
+        System.out.println(isequal);
+
+
+        //if --else statement
+
+        int age=20;
+        if (age>=18)
+
+        {
+            System.out.println("sorry you cant go for the ride");
+            }
+
+        int x=30;
+        int y=15;
+        if (x>y){
+            System.out.println("x is greater than Y");
+        }
+
+        int time=20;
+        if (time>10){
+            System.out.println("good night");
+        }else {
+            System.out.println("good morning");
+        }
+
+//addition  and substraction
+       int num=50;
+       int num2=30;
+         int addition=num+num2;
+        System.out.println("addition is :"+addition);
+        System.out.println("substraction is " +(num-num2));
+        System.out.println("multiplication is :"+(num*num2));
+        System.out.println("divisiom is :"+(num2/2.5));
+
+
+
+       // switch case
+
+     double num1=50,num2=2,result=0;
+     char oper='+';
+     switch (oper){
+         case'+':
+         result=num1+num2;
+         break;
+
+         case'-':
+         result=num1-num2;
+         break;
+
+         case '*':
+             result=num1*num2;
+             break;
+         case'/':
+             result=num1/num2;
+             break;
+
+         default:
+             System.out.println(oper+ "is not supported");
+     }
+        System.out.println(result);
+
+
+
+
+     //sting>>>
+        String Name="Sudipta";
+        Name.concat("Mishra");
+        System.out.println(Name);
+
+
+    String name="Sudipta Mishra";
+       System.out.println(name);
+    String MyName="Alex";
+    String MySurname="Johnson";
+        System.out.println(MyName.concat(MySurname));
+    */
+
+    int num1=10;
+    int num2=20;
+    int num3=15;
+        System.out.println("The average of the 3 Numbers is:" + (num1+num2+num3)/3);
+
+    }
+
+}
