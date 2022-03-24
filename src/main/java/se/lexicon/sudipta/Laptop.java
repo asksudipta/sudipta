1fabce996653001cdaca84e5363c16d17f599dd6
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-24 12:12:03 +0100

    Create new class laptop in this package

---
+++ src/main/java/se/lexicon/sudipta/Laptop.java
@@ -0,0 +1,12 @@
+package se.lexicon.sudipta;
+
+//create a laptop class in one package and call it in app class..
+public class Laptop {
+    String name;
+    String brand;
+    String color;
+    int ram;
+
+
+
+}
