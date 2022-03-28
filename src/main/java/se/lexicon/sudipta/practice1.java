e1c5bb50ec1550fd43780114740e78b4e46de0f9
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-28 12:49:35 +0200

    Guessing concept

---
+++ src/main/java/se/lexicon/sudipta/practice1.java
@@ -0,0 +1,66 @@
+package se.lexicon.sudipta;
+
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+
+public class practice1 {
+    public static void main(String[] args) {
+
+/*
+    String firstName="sudipta";
+    System.out.println(firstName);
+
+
+         //Scanner operation String:
+
+
+         Scanner scan=new Scanner(System.in);
+         System.out.println("Enter your First Name");
+         String firstName=scan.next();
+         System.out.println("Enter your Last Name");
+         String lastName=scan.next();
+         System.out.println("");
+         System.out.println("Enter your age");
+         int age=scan.nextInt();
+         System.out.println("Your full Name is :"+firstName+" "+lastName+" And your Age is :"+age);
+
+*/
+
+        // Scanner operation in Integer:
+
+   /*     Scanner scan = new Scanner(System.in);
+        System.out.println("Enter  First number ");
+        int firstNum = scan.nextInt();
+        System.out.println("Enter Second Number");
+        int secondNum = scan.nextInt();
+
+        System.out.println("Enter Operator(+,-,/,*)");
+        String operation = scan.next();
+
+        switch (operation) {
+            case "+":
+                int add = (firstNum + secondNum);
+                System.out.println("Addition Result is :" + add);
+                break;
+            case "-":
+                int sub = (firstNum - secondNum);
+                System.out.println("Subtraction Result is :" + sub);
+                break;
+            case "/":
+                int div = (firstNum / secondNum);
+                System.out.println("The division Result is :" + div);
+            case "*":
+                int mul = (firstNum * secondNum);
+                System.out.println("The multiplication Result is :" + mul);
+            default:
+                System.out.println("Enter Operator is not valid");
+
+        }
+
+
+*/
+
+
+
+    }
+}
