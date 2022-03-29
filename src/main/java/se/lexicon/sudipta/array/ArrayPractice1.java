c81355a095f9865a6989d0a62a06b1805b0eeb8f
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-29 15:30:06 +0200

    Array string practicing

---
+++ src/main/java/se/lexicon/sudipta/array/ArrayPractice1.java
@@ -0,0 +1,13 @@
+package se.lexicon.sudipta.array;
+
+public class ArrayPractice1 {
+    public static void main(String[] args) {
+
+        String [] cars={"Volvo","Ford","Bmw"};
+        System.out.println(cars[0]);
+        cars[0]="Audi";
+        System.out.println(cars[0]);
+    }
+
+
+    }
