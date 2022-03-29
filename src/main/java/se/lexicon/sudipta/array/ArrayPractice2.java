90b0f94deb755278509ba4ac2e3f9c3c86b0738b
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-29 15:32:49 +0200

    Array length and position

---
+++ src/main/java/se/lexicon/sudipta/array/ArrayPractice2.java
@@ -0,0 +1,15 @@
+package se.lexicon.sudipta.array;
+
+public class ArrayPractice2 {
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+        System.out.println("The length is "+names.length); //3
+        names[0] = "Fredrik";
+        names[2] = "Jonas";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+
+}
