a7a0b2d56fe6e2593d871dc8beb9b846e5339b60
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 14:53:13 +0200

    Index Position  checking for a letter

--- src/main/java/se/lexicon/sudipta/string/IndexPosition.java
+++ src/main/java/se/lexicon/sudipta/string/IndexPosition.java
@@ -2,12 +2,23 @@ package se.lexicon.sudipta.string;
 
 public class IndexPosition {
     public static void main(String[] args) {
-        String position="Long example sentence";
-     for (int i=6 ; i<position.length() ;i=i++){
 
-         System.out.println(position.charAt(i));
-         break;
-     }
+        System.out.println("The index number o is  : ");
+        ex1();
+
+        String position = "Long example sentence";
+        for (int i = 6; i < position.length(); i = i++) {
+
+            System.out.println("The character on Index position  6 is  :"+ position.charAt(i));
+            break;
+        }
+
+    }
+
+    public static void ex1() {
+        String sentence = "Even longer example sentence";
+        int index = sentence.indexOf("o");
+        System.out.println(index);
 
     }
 
