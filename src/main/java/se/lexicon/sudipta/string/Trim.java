4505d90c5c4f887d62a05cb47bcbe1eae6014a64
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 16:55:18 +0200

    Trimming the txt

---
+++ src/main/java/se/lexicon/sudipta/string/Trim.java
@@ -0,0 +1,11 @@
+package se.lexicon.sudipta.string;
+
+public class Trim {
+    public static void main(String[] args) {
+        String text="\tJ\ta\tv\ta\t";
+
+        String str=text.trim();
+        System.out.println(str);
+
+    }
+}
