872b89eac69f6db5b460dd02c5cf677bdcc05ba9
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 16:20:06 +0200

    Replacing test.

---
+++ src/main/java/se/lexicon/sudipta/string/Replacing.java
@@ -0,0 +1,11 @@
+package se.lexicon.sudipta.string;
+
+public class Replacing {
+    public static void main(String[] args) {
+
+        String word="Java is the worst programming language!";
+        System.out.println(word);
+        System.out.println(word.replace("worst" ,"Best"));
+
+    }
+}
