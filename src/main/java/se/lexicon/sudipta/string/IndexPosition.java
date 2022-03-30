cf6dce11c52d9b0711223b7bfc31a5d9ac6a6f0b
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 11:50:31 +0200

    Index Position  checking

---
+++ src/main/java/se/lexicon/sudipta/string/IndexPosition.java
@@ -0,0 +1,14 @@
+package se.lexicon.sudipta.string;
+
+public class IndexPosition {
+    public static void main(String[] args) {
+        String position="Long example sentence";
+     for (int i=6 ; i<position.length() ;i=i++){
+
+         System.out.println(position.charAt(i));
+         break;
+     }
+
+    }
+
+}
