5d9605256fd34929d1151e7da63ea3f07f3dd765
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 11:01:26 +0200

    String length count

--- src/main/java/se/lexicon/sudipta/string/StringLength.java
+++ src/main/java/se/lexicon/sudipta/string/StringLength.java
@@ -1,5 +1,11 @@
 package se.lexicon.sudipta.string;
 
 public class StringLength {
+    public static void main(String[] args) {
+        String character="Java";
+        int num=character.length();
+        System.out.println("The character is :"+character);
+        System.out.println("The length of the character is :"+num);
 
+    }
 }
