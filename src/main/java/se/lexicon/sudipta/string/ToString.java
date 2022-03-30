69c9ee64a98ba16377969920093d3769d1b0355d
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-30 23:40:59 +0200

    Parse the intiger to string

---
+++ src/main/java/se/lexicon/sudipta/string/ToString.java
@@ -0,0 +1,10 @@
+package se.lexicon.sudipta.string;
+
+public class ToString {
+    public static void main(String[] args) {
+        int i=20;
+        System.out.println("The main value is --:"+i);
+        String Str=String.valueOf(i);
+        System.out.println("The Added value is --:"+i+20);
+    }
+}
