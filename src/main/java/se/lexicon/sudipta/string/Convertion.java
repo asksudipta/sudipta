019d9b67d448197a396f10cf667fd48e6794271c
Sandip Kumar Mohapatra <sandipkumar.mohapatra@ingka.com>
2022-03-31 08:46:26 +0200

    Convert the character to String..

---
+++ src/main/java/se/lexicon/sudipta/string/Convertion.java
@@ -0,0 +1,10 @@
+package se.lexicon.sudipta.string;
+
+public class Convertion {
+    public static void main(String[] args) {
+        char[] tests = {'J','a','v','a'};
+        System.out.println("The letters are --:"+tests);
+        String sentence=String.valueOf(tests);
+        System.out.println("The full word is  :"+sentence);
+    }
+}
